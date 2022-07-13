a6d6eb2ce5a52b159e6d4ba62ab95e03d5e7a861
Luis <sergioluispagano@gmail.com>
2022-07-13 18:39:33 -0300

    Puede_tener_chequera

--- Clases/Cliente/cliente_black.py
+++ Clases/Cliente/cliente_black.py
@@ -6,3 +6,9 @@ class ClienteBlack(Cliente):
 
     def puede_tener_chequera(self):
         return True
+
+    def puede_crear_tarjeta_credito(self):
+        return True
+
+    def puede_comprar_dolar(self):
+        return True
--- Clases/Cliente/cliente_gold.py
+++ Clases/Cliente/cliente_gold.py
@@ -7,3 +7,9 @@ class ClienteGold(Cliente):
 
     def puede_tener_chequera(self):
         return True
+
+    def puede_crear_tarjeta_credito(self):
+        return True
+
+    def puede_comprar_dolar(self):
+        return True
